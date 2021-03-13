a84b4606eec14637eb51c99362f8f3d567a23cd6
Bulkgi <73314476+Bulkgi@users.noreply.github.com>
2021-03-13 17:36:12 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :farmer_users
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'  
+  }
+  devise_for :farmer_users, controllers: {
+    sessions: 'farmer_users/sessions',
+    passwords: 'farmer_users/passwords',
+    registrations: 'farmer_users/registrations'
+  }
+
   root to: 'products#index'
   resources :products, only: :index
 end
