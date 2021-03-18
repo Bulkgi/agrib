fc7d21f3c466bdce92fbea571da0b607f5ed52de
Bulkgi <73314476+Bulkgi@users.noreply.github.com>
2021-03-18 10:23:09 +0900

    userとfarmer_userのストロングパラメーターを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    passwords: 'users/passwords',
     registrations: 'users/registrations'  
   }
   devise_for :farmer_users, controllers: {
     sessions: 'farmer_users/sessions',
-    passwords: 'farmer_users/passwords',
     registrations: 'farmer_users/registrations'
   }
+
 
   root to: 'products#index'
   resources :products, only: :index
