d34c955a673a4c6f8dd9c5035dea6282b61fcbf6
Bulkgi <73314476+Bulkgi@users.noreply.github.com>
2021-03-23 17:43:06 +0900

    トップページ編集

--- app/models/farmer_user.rb
+++ app/models/farmer_user.rb
@@ -6,7 +6,7 @@ class FarmerUser < ApplicationRecord
 
   validates :password,         format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは6文字以上の半角英数字にしてください'}
   with_options presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name       
+    validates :name       
   end
   with_options presence: true, format:{ with: /\A[ァ-ヶ一-]+\z/, message: '全角カナ文字を使用してください' } do
     validates :name_ruby 
